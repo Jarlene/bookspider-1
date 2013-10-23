2b428d57c85ae64195edb44f17a654d6854002da
i guest <iguest@sogou.com>
2013-10-23 19:07:48 +0800

    增加comment_uri, comment, icon字段

--- www/api.php
+++ www/api.php
@@ -1,43 +1,15 @@
 <?php
 	require("php/db.inc");
 	require("php/util.inc");
+	require("php/api.inc");
 
-	$api = php_reqvar("api", "top");
-	$api(); // call function
-
-	function top()
-	{
-		$db = dbopen("joke");
-		if($db->connect_errno)
-		{
-			echo "mysql error " . $db->connect->error;
-			return;
-		}
-
-		$page = php_reqvar("page", "1");
-		$page = $page < 1 ? 1 : $page;
-		$sql = sprintf("select * from joke limit %d,50", ($page-1)*50);
-		$res = $db->query($sql);
-
-		$jokes = array();
-		while($row = $res->fetch_assoc())
-		{
-			$joke = array();
-			$joke["author"] = $row["author"];
-			$joke["datetime"] = $row["datetime"];
-			$joke["content"] = $row["content"];
-			$joke["image"] = $row["image"];
-			$joke["approve"] = $row["approve"];
-			$joke["disapprove"] = $row["disapprove"];
-			$jokes[] = $joke; // add book
-		}
-		$res->free();
-
-		echo json_encode($jokes);
-	}
-
-	function tip()
-	{
-		echo "api.php?api=search/top";
-	}
+	$page = php_reqvar("page", "1");
+	$content = php_reqvar("content", "0");
+	$range = php_reqvar("range", "0");
+	$order = php_reqvar("order", "0");
+	$timestamp = php_reqvar("s", time());
+	$limit = php_reqvar("limit", 50);
+	
+	$jokes = joke_query($page, $order, $range, $content, $timestamp);
+	echo json_encode($jokes);
 ?>
