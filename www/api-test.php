0f37e53e0ff5a64ebf23a0a15965ef5be65031a7
i guest <iguest@sogou.com>
2013-10-30 15:29:40 +0800

    add comment.html

--- www/api-test.php
+++ www/api-test.php
@@ -3,27 +3,19 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Joke</title>
 <link rel="stylesheet" type="text/css" href="css/style.css" /> 
+<script src="js/xml.js" type="text/javascript"></script>
 </head>
 <body>
-<div align="right" style="margin: 5px">
-	<form method="post" action="search.php">
-		search:
-		<input type="text" name="search" />
-		<input type="submit" value="Search" />
-	</form>
-</div>
-
 <div id="books">
 <table align="center" width="98%">
 	<tr style="background-color: #CCCCCC">
-		<td width="120px" align="center"><a href="#">作者</a></td>
-		<td width="150px" align="center"><a href="#">时间</a></td>
-		<td width="50px" align="center"><a href="#">赞成</a></td>
-		<td width="50px" align="center"><a href="#">反对</a></td>
-		<td width="50px" align="center"><a href="#">评论</a></td>
-		<td width="20%" align="center"><a href="#">笑话</a></td>
-		<td width="120px" align="center"><a href="#">图片</a></td>
-		<td width="120px" align="center"><a href="#">图标</a></td>
+		<td width="100px" align="center"><a href="#">作者</a></td>
+		<td width="160px" align="center"><a href="#">时间</a></td>
+		<td width="40px" align="center"><a href="#">赞成</a></td>
+		<td width="40px" align="center"><a href="#">反对</a></td>
+		<td width="40px" align="center"><a href="#">评论</a></td>
+		<td align="center"><a href="#">笑话</a></td>
+		<td width="300px" align="center"><a href="#">图片</a></td>
 	</tr>
 
 <?php
@@ -44,14 +36,16 @@
 	for($i=0; $i<count($jokes); $i++)
 	{
 		echo "<tr>";
-		echo "<td>" . $jokes[$i]["author"] ."</td>";
+		echo "<td>";
+		if(strlen($jokes[$i]["icon"]) > 0)
+			echo "<image src=\"" . $jokes[$i]["icon"] . "\" />";
+		echo $jokes[$i]["author"] ."</td>";
 		echo "<td>" . $jokes[$i]["datetime"] ."</td>";
 		echo "<td>" . $jokes[$i]["approve"] ."</td>";
 		echo "<td>" . $jokes[$i]["disapprove"] ."</td>";
-		echo "<td><a href=\"" . $jokes[$i]["comment_uri"] . "\">" . $jokes[$i]["comment"] ."</a></td>";
+		echo "<td><a href=\"/joke/comment.html?id=" . $jokes[$i]["id"] . "\">" . $jokes[$i]["comment"] ."</a></td>";
 		echo "<td>" . $jokes[$i]["content"] ."</td>";
 		echo "<td>" . $jokes[$i]["image"] ."</td>";
-		echo "<td>" . $jokes[$i]["icon"] ."</td>";
 		echo "</tr>\n";
 	}
 	$res->free();
