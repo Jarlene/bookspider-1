e456edef29dd8acb0832455401fccb207625fa6c
i guest <iguest@sogou.com>
2013-11-05 16:46:18 +0800

    增加联系人字段

--- www/feedback.php
+++ www/feedback.php
@@ -3,14 +3,15 @@
 	require("php/util.inc");
 
 	$user = php_reqvar("u", "");
-	$feedback = php_reqvar("c", "");
-	joke_feedback($user, $feedback);
+	$contact = php_reqvar("c", "");
+	$feedback = php_reqvar("f", "");
+	joke_feedback($user, $contact, $feedback);
 	
-	function joke_feedback($user, $feedback)
+	function joke_feedback($user, $contact, $feedback)
 	{
 		// items
 		$datetime = date_format(date_create(), 'Y-m-d H:i:s');
-		$sql = sprintf("insert into feedback (user, datetime, content) values ('%s', '%s', '%s')", $user, $datetime, $feedback);
+		$sql = sprintf("insert into feedback (user, datetime, contact, content) values ('%s', '%s', '%s', '%s')", $user, $datetime, $contact, $feedback);
 		
 		// query
 		$db = dbopen("joke");
