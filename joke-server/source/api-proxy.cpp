6a0838c108f53138bdd2322deaf8cd86bd0e7a2d
i guest <iguest@sogou.com>
2013-11-05 09:13:10 +0800

    proxy api

---
+++ joke-server/source/api-proxy.cpp
@@ -0,0 +1,70 @@
+#include "web-session.h"
+#include "http-server.h"
+#include "http-proxy.h"
+#include "cppstringext.h"
+
+int config_proxy_save();
+
+int WebSession::OnProxy()
+{
+	char msg[128] = {0};
+	const char* from = http_server_get_header(m_http, "x-forward-for");
+	if(!from)
+		from = m_ip.c_str();
+	snprintf(msg, sizeof(msg), "Hello Proxy: %s", from);
+
+	return Reply(std::string(msg));
+}
+
+int WebSession::OnAddProxy()
+{
+	std::string proxy;
+	if(!m_params.Get("proxy", proxy))
+		return Reply(ERROR_PARAM, "miss proxy");
+
+	std::vector<std::string> proxies;
+	std::vector<std::string>::iterator it;
+	Split(proxy.c_str(), ',', proxies);
+	for(it = proxies.begin(); it != proxies.end(); ++it)
+	{
+		std::string host = Strip(it->c_str());
+		http_proxy_add(host.c_str());
+	}
+
+	config_proxy_save(); //save to file
+	return Reply(0, "OK");
+}
+
+int WebSession::OnDelProxy()
+{
+	std::string proxy;
+	if(!m_params.Get("proxy", proxy))
+		return Reply(ERROR_PARAM, "miss proxy");
+
+	std::vector<std::string> proxies;
+	std::vector<std::string>::iterator it;
+	Split(proxy.c_str(), ',', proxies);
+	for(it = proxies.begin(); it != proxies.end(); ++it)
+	{
+		std::string host = Strip(it->c_str());
+		http_proxy_delete(host.c_str());
+	}
+
+	config_proxy_save(); //save to file
+	return Reply(0, "OK");
+}
+
+static void OnListProxyHelper(void* param, const char* value)
+{
+	jsonarray* json = (jsonarray*)param;
+	json->add(value);
+}
+
+int WebSession::OnListProxy()
+{
+	jsonarray json;
+	http_proxy_list(OnListProxyHelper, &json);
+
+	std::string reply = json.json();
+	return Reply(reply.c_str());
+}
---
+++ joke-server/source/config-proxy.cpp
@@ -0,0 +1,40 @@
+#include "http-proxy.h"
+#include "cppstringext.h"
+#include <string>
+#include <stdio.h>
+
+int config_proxy_load()
+{
+	FILE* fp = fopen("data/proxy.cfg", "r");
+	if(!fp)
+		return errno;
+
+	char proxy[128] = {0};
+	while(fgets(proxy, sizeof(proxy)-1, fp))
+	{
+		std::string host = Strip(proxy, " \r\n");
+		http_proxy_add(host.c_str());
+	}
+
+	fclose(fp);
+	return 0;
+}
+
+static void OnListProxyHelper(void* param, const char* value)
+{
+	FILE* fp = (FILE*)param;
+	fwrite(value, 1, strlen(value), fp);
+	fwrite("\r\n", 1, 2, fp);
+}
+
+int config_proxy_save()
+{
+	FILE* fp = fopen("data/proxy.cfg", "w");
+	if(!fp)
+		return errno;
+
+	http_proxy_list(OnListProxyHelper, fp);
+
+	fclose(fp);
+	return 0;
+}
