26a86a8e9fd6fab46b3bbfd532633c72026f7a12
ireader <chinatao3@gmail.com>
2014-01-18 21:35:22 +0800

    php web spider

---
+++ www/zol-catalog-parser.php
@@ -0,0 +1,53 @@
+<?php
+	require("php/http.inc");
+	require("php/dom.inc");
+
+	function zol_wallpaper_subcatalog($uri)
+	{
+		$response = http_get($uri);
+		$doc = dom_parse($response);
+		$elements = xpath_query($doc, "//dl[@class='filter-item clearfix subcateClass']/dd");
+
+		$catalog = array();
+		$catalog['all'] = $uri;
+
+		if (!is_null($elements)) {
+			foreach ($elements as $element) {
+				$nodes = $element->childNodes;
+				foreach ($nodes as $node) {
+					if(XML_ELEMENT_NODE == $node->nodeType){
+						$href = $node->getAttribute('href');
+						if(strlen($href) > 0){
+							$catalog[$node->nodeValue] = $href;
+						}
+					}
+				}
+			}
+		}
+		
+		return $catalog;
+	}
+
+	$uri = 'http://sj.zol.com.cn/bizhi/640x1136/';
+	$response = http_get($uri);
+
+	$doc = dom_parse($response);
+	$elements = xpath_query($doc, "//dl[@class='filter-item first clearfix']/dd[@class='brand-sel-box clearfix']");
+
+	$catalog = array();
+	if (!is_null($elements)) {
+		foreach ($elements as $element) {
+			$nodes = $element->childNodes;
+			foreach ($nodes as $node) {
+				if(XML_ELEMENT_NODE == $node->nodeType){
+					$href = $node->getAttribute('href');
+					if(strlen($href) > 0){
+						$catalog[$node->nodeValue] = zol_wallpaper_subcatalog('http://sj.zol.com.cn' . $href);
+					}
+				}
+			}
+		}
+	}
+	
+	print_r($catalog);
+?>
