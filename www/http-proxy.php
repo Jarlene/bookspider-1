1b40e3feb4c64bbc9e6e99732072bd4ba33e41f3
i guest <iguest@sogou.com>
2014-03-17 19:34:22 +0800

    1. fixed http user-agent value 2. http-proxy use multiple connection acceleration

--- www/http-proxy.php
+++ www/http-proxy.php
@@ -13,16 +13,41 @@ function http_proxy_post($uri, $data, $pattern, $timeout=20, $cfg="proxy.cfg", $
 
 class HttpProxy
 {
-	function __construct($proxy="proxy.cfg")
+	function __construct($proxy)
 	{
-		$this->m_proxies = split(",", file_get_contents("proxy.cfg"));
+		function_exists('curl_init') || die('CURL Library Not Loaded');     //exit if don't install curl
+
+		$this->m_proxies = split(",", file_get_contents($proxy));
+
+		$this->m_options = array(
+			'port' => 80,
+			'timeout' => 20,
+			'cookie' => false,
+			'ssl' => false,
+			'gzip' => true,
+			'proxy' => false
+		);
+
+		$this->m_headers = array( // http headers
+			'User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64; rv:27.0) Gecko/20100101 Firefox/27.0',
+			'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+			'Accept-Encoding: gzip, deflate',
+			'Accept-Language: en-US,en;q=0.5'
+		);
+
+		$this->m_multi = curl_multi_init();
 	}
-	
+
+	function __destruct()
+	{
+		curl_multi_close($this->m_multi);
+	}
+
 	function get($uri, $pattern, $timeout=20, $headers=array())
 	{
 		return $this->_request("GET", $uri, null, $pattern, $timeout, $headers);
 	}
-	
+
 	function post($uri, $data, $pattern, $timeout=20, $headers=array())
 	{
 		return $this->_request("POST", $uri, $data, $pattern, $timeout, $headers);
@@ -30,41 +55,149 @@ class HttpProxy
 
 	private function _request($method, $url, $data, $pattern, $timeout, $headers)
 	{
-		$base = time() * 6;
+		$j = time() * 6;
 
-		$urls = array();
-		$datum = array();
-		$proxies = array();
+		$curls = array();
 		for($i  = 0; $i < 6 && $i < count($this->m_proxies); $i++){
-			$urls[] = $url;
-			$datum[] = $data;
-			$proxies[] = $this->m_proxies[($base + $i) % count($this->m_proxies)];
+			$curl = curl_init($url);
+			$this->_setopt($method, $curl, $data ? $data : null, $headers);
+
+			// set proxy
+			$host = $this->m_proxies[($j + $i) % count($this->m_proxies)];
+			$proxy = split(":", $host);
+			if(count($proxy) > 1){
+				curl_setopt($curl, CURLOPT_PROXYPORT, $proxy[1]);
+			} else {
+				curl_setopt($curl, CURLOPT_PROXYPORT, 80);
+			}
+			curl_setopt($curl, CURLOPT_PROXY, $proxy[0]);
+			curl_setopt($curl, CURLOPT_PROXYTYPE, CURLPROXY_HTTP);
+
+			curl_multi_add_handle($this->m_multi, $curl);
+			$curls[] = $curl;
 		}
 
-		$result = array();
-		$http = new HttpMultiple();
-		$http->setproxy($proxies);
-		if($method == "POST"){
-			$r = $http->post($urls, $datum, array($this, '__OnHttp'), &$result, $timeout, $headers);
-		} else {
-			$r = $http->get($urls, array($this, '__OnHttp'), &$result, $timeout, $headers);
+		$r = $this->_perform($this->m_multi, $timeout, $pattern);
+
+		foreach($curls as $curl){
+			curl_multi_remove_handle($this->m_multi, $curl);
+			curl_close($curl);
 		}
 
-		foreach($result as $v){
-			if(stripos($v, $pattern)){
+		return $r;
+	}
+
+	private function _perform($multi, $timeout, $pattern)
+	{
+		$t0 = gettimeofday(true);
+
+		// running
+		do{
+			do{
+				$status = curl_multi_exec($multi, $active);
+			} while($status === CURLM_CALL_MULTI_PERFORM);
+
+			$t1 = gettimeofday(true);
+			$t = ($t0 + $timeout < $t1) ? 0 : ($timeout - ($t1 - $t0));			
+			if(curl_multi_select($multi, $timeout<0?-1:$t) < 1) {
+				// log timeout
+				return False;
+			}
+
+			$v = $this->_read($multi, $pattern);
+			if($v){
 				return $v;
 			}
+
+		} while($status === CURLM_OK && $active);
+
+		if(CURLM_OK !== $status || 0 !== $active){
+			// log
 		}
-		return "";
+		return False;
 	}
 
-	function __OnHttp($result, $idx, $r, $header, $body)
+	private function _read($multi, $pattern)
 	{
-		if(0 == $r){
-			$result[] = $body;
+		$info = curl_multi_info_read($multi);
+		while($info && CURLMSG_DONE===$info["msg"] && CURLE_OK==$info["result"]){
+			$curl = $info["handle"];
+			$response = curl_multi_getcontent($curl);
+			$headerSize = curl_getinfo($curl, CURLINFO_HEADER_SIZE);
+			if($headerSize > 0 && strlen($response) >= $headerSize){
+				//$header = substr($response, 0, $headerSize);
+				$body = substr($response, $headerSize);
+				if(stripos($body, $pattern)){
+					return $body;
+				}
+			}
+
+			// next
+			$info = curl_multi_info_read($multi);
+		}
+
+		return False;
+	}
+
+	private function _setopt($method, $curl, $data, $headers)
+	{
+		$options = $this->m_options;
+		curl_setopt($curl, CURLOPT_PORT, $options['port']); // HTTP default port
+		curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true); // enable HTTP Location header
+		//curl_setopt($curl, CURLOPT_USERAGENT, $this->m_headers['User-Agent']);  // default user-agent
+		//curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, $options['timeout']); // connection timeout
+		//curl_setopt($curl, CURLOPT_TIMEOUT, $options['timeout']); // execute timeout
+		curl_setopt($curl, CURLOPT_HEADER, true); // get HTTP header
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true); // get HTTP body
+		curl_setopt($curl, CURLOPT_BINARYTRANSFER, true); // binary content
+		curl_setopt($curl,CURLOPT_ENCODING, ''); // decode gzip
+
+		// proxy
+		// if($options['proxy']){
+			// $proxyType = $options['proxyType']=='HTTP' ? CURLPROXY_HTTP : CURLPROXY_SOCKS5;
+			// curl_setopt($curl, CURLOPT_PROXYTYPE, $proxyType);
+			// curl_setopt($curl, CURLOPT_PROXY, $options['proxyHost']);
+			// curl_setopt($curl, CURLOPT_PROXYPORT, $options['proxyPort']);
+
+			// if($options['proxyAuth']){
+				// $proxyAuthType = $options['proxyAuthType']=='BASIC' ? CURLAUTH_BASIC : CURLAUTH_NTLM;  
+                // $proxyUser = "[{$options['proxyAuthUser']}]:[{$options['proxyAuthPwd']}]";  
+				// curl_setopt($curl, CURLOPT_PROXYAUTH, $proxyAuthType);  
+                // curl_setopt($curl, CURLOPT_PROXYUSERPWD, $proxyUser);  
+			// }
+		// }
+
+		// ssl
+		if($options['ssl']){
+			curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+			curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, true);
+		}
+
+		// cookie
+		if($options['cookie']){
+			$cookfile = tempnam(sys_get_temp_dir(), 'curl-cookie-');
+			curl_setopt($curl, CURLOPT_COOKIEJAR, $cookfile);
+            curl_setopt($curl, CURLOPT_COOKIEFILE, $cookfile);
+		}
+	
+		foreach($this->m_headers as $header){
+			$headers[] = $header;
+		}
+
+//		curl_setopt($curl, CURLOPT_URL, $uri);
+		curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+
+		if($method == "POST" && $data){
+			curl_setopt($curl, CURLOPT_POST, true);
+			curl_setopt($curl, CURLOPT_POSTFIELDS, $data);	
 		}
 	}
-	
+
+	private $m_multi = null;
 	private $m_proxies = null;
+	private $m_options = null;
+	private $m_headers = null;
 }
+
+//print_r(http_proxy_get("http://www.pingshu8.com/play_23925.html", "luckyzz@163.com"));
 ?>
